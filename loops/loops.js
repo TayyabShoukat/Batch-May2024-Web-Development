5e4a44ea09e5ca80b7d0953c3c30eb704a781fb0
Muhammad Tayyab Shoukat <100590476+TayyabShoukat@users.noreply.github.com>
2024-05-25 21:47:40 +0500

    Loops

--- loops/loops.js
+++ loops/loops.js
@@ -27,7 +27,34 @@
 // }
 // while (i <= 10);
 // console.log("bye");
-let n = 1;
-for (n = 1; n <= 5; n++) {
-    console.log("For Loop");
+// let n: number = 1;
+// for (n = 1; n <= 5; n++)
+// {
+//   console.log("For Loop");
+// }
+// 2;
+// 4;
+// 6;
+// 8;
+// 10;
+// starting point => 2
+// increment => 2
+// ending point => 10
+// let n: number;
+// for (n = 2; n <= 10; n = n + 2)
+// {
+//   console.log(n);
+// }
+// 20;
+// 40;
+// 60;
+// 80;
+// 100;
+// 120;
+// starting point => 20
+// increment => 20
+// ending point => 120
+let n;
+for (n = 20; n <= 120; n = n + 20) {
+    console.log(n);
 }
