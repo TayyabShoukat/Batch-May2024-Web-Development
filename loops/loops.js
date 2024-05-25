e3de0ce9e4f1ac0aefcf8b7714d3d482dd5f6d43
Muhammad Tayyab Shoukat <100590476+TayyabShoukat@users.noreply.github.com>
2024-05-25 21:42:02 +0500

    Loops

--- loops/loops.js
+++ loops/loops.js
@@ -1,2 +1,33 @@
 "use strict";
-console.log("Hello World");
+// let i: number = 1;
+// while (i <= 5) {
+//   console.log("hello world");
+//   i++;
+// }
+// console.log("bye");
+// let n: number = 1;
+// while (n <= 10)
+// {
+//   console.log(`${2} X ${n} = ${2 * n}`);
+//   n++;
+// }
+// let i: number = 1;
+// do
+// {
+//   console.log("hello world");
+//   i++;
+// }
+// while (i <= 0);
+// console.log("bye");
+// let i: number = 1;
+// do
+// {
+//   console.log("hello world");
+//   i++;
+// }
+// while (i <= 10);
+// console.log("bye");
+let n = 1;
+for (n = 1; n <= 5; n++) {
+    console.log("For Loop");
+}
