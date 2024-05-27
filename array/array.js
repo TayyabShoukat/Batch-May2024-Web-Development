8d40a9f775edf79eb638543dd479196449a5ebf9
Muhammad Tayyab Shoukat <100590476+TayyabShoukat@users.noreply.github.com>
2024-05-27 21:58:38 +0500

    Array

---
+++ array/array.js
@@ -0,0 +1,40 @@
+"use strict";
+// let arr: number[] = [];
+// arr[0] = 25;
+// arr[1] = 35;
+// arr[2] = 40;
+// arr[3] = 50;
+// arr[4] = 60;
+// console.log(`${arr[0]}`);
+// console.log(`${arr[1]}`);
+// console.log(`${arr[2]}`);
+// console.log(`${arr[3]}`);
+// console.log(`${arr[4]}`);
+// let arr: number[] = [25, 35, 40, 50, 60];
+// console.log(`${arr[0]}`);
+// console.log(`${arr[1]}`);
+// console.log(`${arr[2]}`);
+// console.log(`${arr[3]}`);
+// console.log(`${arr[4]}`);
+//  0   1   2   3   4
+// let arr: number[] = [25, 35, 40, 50, 60];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(`${arr[i]}`);
+// }
+// let arr1: number[] = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+// for (let i = 0; i < arr1.length; i++) {
+//   console.log(`${arr1[i]}`);
+// }
+// let names: string[] = ["Faisalabad", "Lahore", "Islamabad", "Karachi"];
+// for (let i = 0; i < names.length; i++) {
+//   console.log(`${names[i]}`);
+// }
+// let names: string[] = ["Faisalabad", "Lahore", "Islamabad", "Karachi"];
+// let result = names.map((items) => items);
+// console.log(result);
+//  0       1      2
+//    0,1,2   0,1,2   0,1,2
+let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log(`${matrix[1][1]}`);
+console.log(`${matrix[2][2]}`);
+console.log(`${matrix[0][2]}`);
