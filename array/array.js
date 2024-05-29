72133eae9ed66f4d894fe7067e99cd81f7ddbecd
Muhammad Tayyab Shoukat <100590476+TayyabShoukat@users.noreply.github.com>
2024-05-29 22:00:29 +0500

    map and filter

--- array/array.js
+++ array/array.js
@@ -34,7 +34,44 @@
 // console.log(result);
 //  0       1      2
 //    0,1,2   0,1,2   0,1,2
-let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-console.log(`${matrix[1][1]}`);
-console.log(`${matrix[2][2]}`);
-console.log(`${matrix[0][2]}`);
+// let matrix: number[][] = [[1,2,3],[4,5,6],[7,8,9]]
+// console.log(`${matrix[1][1]}`)
+// console.log(`${matrix[2][2]}`)
+// console.log(`${matrix[0][2]}`)
+// 0    1   2  3
+// let arr: number[] = [20, 35, 45, 90];
+// console.log(`${arr[0]}`);
+// console.log(`${arr[1]}`);
+// console.log(`${arr[2]}`);
+// console.log(`${arr[3]}`);
+// 0    1   2  3
+// let arr: number[] = [20, 35, 45, 90];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(`${arr[i]}`);
+// }
+// array latest loop => map()
+// let arr: number[] = [20, 35, 45, 90];
+// let result = arr.map((items) => items);
+// console.log(result);
+// // 0     1     2       3
+// let arr: string[] = ["Fsd", "Lhr", "Isb", "Pwr"];
+// let results = arr.map((items) => items);
+// console.log(results);
+// let arr: number[] = [2, 4, 5, 3];
+// let result = arr.map((items) => items * 2);
+// console.log(result);
+// let arr: number[] = [6, 8, 10, 11, 13];
+// let result = arr.filter((items) => items % 2 == 0);
+// console.log(result);
+// let names: string[] = ["Fsd", "Lhr", "Isb", "Psw"];
+// let result = names.filter((items) => items == "Lhr");
+// console.log(result);
+let arr1 = [0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 10, 100, 2000];
+let result1 = arr1.filter((items) => items == 0);
+console.log(result1);
+let arr = [0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 10, 100, 2000];
+let result = arr.map((items) => items * 2);
+console.log(result);
+let names = ["ali", "qasim", "zain", "usama"];
+let result2 = names.filter((items) => items == "ali" || items == "qasim");
+console.log(result2);
