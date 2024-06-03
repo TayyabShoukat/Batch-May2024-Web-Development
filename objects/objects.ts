539ed01519ad94c1816bd763042874c7795961c8
Muhammad Tayyab Shoukat <100590476+TayyabShoukat@users.noreply.github.com>
2024-06-03 21:50:06 +0500

    object

---
+++ objects/objects.ts
@@ -0,0 +1,20 @@
+// let studentObject = {
+//   name: "ali",
+//   age: 30,
+//   city: "Fsd",
+// };
+
+// // console.log(studentObject);
+// console.log(studentObject.name);
+// console.log(studentObject.age);
+// console.log(studentObject.city);
+
+let colors = {
+  color1: "red",
+  color2: "blue",
+  color3: "green",
+  color4: "Yellow",
+  quantity: 4,
+};
+console.log(colors.color3);
+console.log(colors.quantity)
