c6d685290712b046e3171e6534455e97d9e326ff
Muhammad Tayyab Shoukat <100590476+TayyabShoukat@users.noreply.github.com>
2024-06-07 21:28:16 +0500

    Tuple

---
+++ tuple/tuple.js
@@ -0,0 +1,22 @@
+"use strict";
+// Array
+// let arr: number[] = [2, 4, 6, 8, 10, 12, 16];
+// console.log(arr);
+// Object
+// let stdObj = {
+//   rollnumber: 2,
+//   studentName: "Ali",
+//   class: 12,
+// };
+// console.log(stdObj);
+// Tuple
+// Store Multiple values with Different Data Types with []
+// let ourTuple: [number, boolean, string];
+// ourTuple = [5, false, 'Coding'];
+// console.log(ourTuple)
+// let ourTuple: [string, number, boolean];
+// ourTuple = ["Coding", 5, false];
+// console.log(ourTuple);
+let studentTuple;
+studentTuple = [1, "Ali", "Ramzan", 12, "Fsd"];
+console.log(studentTuple);
---
+++ tuple/tuple.ts
@@ -0,0 +1,26 @@
+// Array
+// let arr: number[] = [2, 4, 6, 8, 10, 12, 16];
+// console.log(arr);
+
+// Object
+// let stdObj = {
+//   rollnumber: 2,
+//   studentName: "Ali",
+//   class: 12,
+// };
+// console.log(stdObj);
+
+// Tuple
+// Store Multiple values with Different Data Types with []
+
+// let ourTuple: [number, boolean, string];
+// ourTuple = [5, false, 'Coding'];
+// console.log(ourTuple)
+
+// let ourTuple: [string, number, boolean];
+// ourTuple = ["Coding", 5, false];
+// console.log(ourTuple);
+
+let studentTuple: [number, string, string, number, string];
+studentTuple = [1, "Ali", "Ramzan", 12, "Fsd"];
+console.log(studentTuple);
